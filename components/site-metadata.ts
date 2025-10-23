883f1b3d68b56deced8ee7f8bb9e2968620eb6b9
Pujon Das Auvi <auvipujondas@gmail.com>
2025-10-23 18:17:55 +0600

    fix: update site description for improved clarity and focus on skills

--- components/site-metadata.ts
+++ components/site-metadata.ts
@@ -1,6 +1,7 @@
 export const SITE_TITLE = "PUJON DAS AUVI"
-export const SITE_DESCRIPTION =
-  "Full Stack Developer | Node.js, React.js, TypeScript, Redis, Docker, AWS | Building Scalable Systems Focused on Performance and User Experience"
+// export const SITE_DESCRIPTION =
+//   "Full Stack Developer | Proficiency in JS, TS, Docker and AWS | Microservices | Cloud & AI Explorer"
 export const SITE_ROLE = "Software Engineer / Full Stack Developer"
-
+export const SITE_DESCRIPTION = "I am a Full Stack Developer with expertise in JavaScript, TypeScript, Docker, and AWS. Focused on scalable microservices, cloud solutions, and AI-driven development."
 export const DEFAULT_SOCIAL_IMAGE = "/logo.png"
+// Software Engineer | 5+ years of exp | Proficiency in Python & JS | Microservices | Cloud & AI Explorer
